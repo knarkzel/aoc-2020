5688104f59d4340f28037d1f0898e7602a691c35
Knarkzel <knarkzel@gmail.com>
2021-11-23 19:40:35 +0100

    day 2

--- day2/main.c
+++ day2/main.c
@@ -1,36 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void part1(int lines, int n[512]) {
-  for (int i = 0; i < lines; i++)
-    for (int j = 0; j < lines; j++)
-      if (n[i] + n[j] == 2020 && i != j) {
-        printf("Part 1: %d\n", n[i] * n[j]);
-        return;
-      }
+typedef struct {
+  int start;
+  int end;
+  char letter;
+  char rest[25];
+} password;
+
+void part1(password *p, int lines) {
+  int valid = 0;
+  for (int i = 0; i < lines; i++) {
+    int count = 0;
+    for (int j = 0; j < strlen(p[i].rest); j++)
+      if (p[i].rest[j] == p[i].letter)
+        count++;
+    if (count >= p[i].start && count <= p[i].end)
+      valid++;
+  }
+  printf("Part 1: %d\n", valid);
 }
 
-void part2(int lines, int n[512]) {
-  for (int i = 0; i < lines; i++)
-    for (int j = 0; j < lines; j++)
-      for (int k = 0; k < lines; k++)
-        if (n[i] + n[j] + n[k] == 2020 && i != j != k) {
-          printf("Part 2: %d\n", n[i] * n[j] * n[k]);
-          return;
-        }
+void part2(password *p, int lines) {
+  int valid = 0;
+  for (int i = 0; i < lines; i++) {
+    char letter = p[i].letter;
+    int start = p[i].start - 1, end = p[i].end - 1;
+    if (p[i].rest[start] == letter ^ p[i].rest[end] == letter)
+      valid++;
+  }
+  printf("Part 2: %d\n", valid);
 }
 
 int main() {
+  char s[50];
   int lines = 0;
-  char s[4096];
-  int n[512] = { 0 };
   FILE *fp = fopen("input.txt", "r");
 
-  while (fgets(s, sizeof s, fp) != NULL) 
-    n[lines++] = atoi(s);
+  password p[1000];
+  while (fgets(s, sizeof s, fp) != NULL) {
+    password temp;
+    sscanf(s, "%d-%d %c: %s\n", &temp.start, &temp.end, &temp.letter, temp.rest);
+    p[lines++] = temp;
+  }
 
-  part1(lines, n);
-  part2(lines, n);
+  part1(p, lines);
+  part2(p, lines);
 
   return 0;
 }
